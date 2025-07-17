8451f6634008b38e09d5414e7574109f5ead5a57
Charukesh Prasanth <charukeshprashanth@gmail.com>
2025-07-17 21:45:47 +0530

    did a stack program

---
+++ 07_Stack_and_Monotonic_Stack/Easy/validparenthesis.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        d={")":"(","}":"{","]":"["}
+        for i in s:
+            if i in d.values():
+                stack.append(i)
+            elif i in d.keys():
+                if not stack or d[i]!=stack.pop():
+                    return False
+        return not stack
+    
